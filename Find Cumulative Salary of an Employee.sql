fd763acfd27c5921d06d745a81e8a2c6e09e06c2
turtlejenny <31515354+turtlejenny@users.noreply.github.com>
2019-02-14 21:40:00 -0800

    Update Find Cumulative Salary of an Employee.sql

--- Find Cumulative Salary of an Employee.sql
+++ Find Cumulative Salary of an Employee.sql
@@ -1,3 +1,4 @@
+-- 579.Find Cumulative Salary of an Employee
 -- The Employee table holds the salary information in a year.
 
 -- Write a SQL to get the cumulative sum of an employee's salary over a period of 3 months but exclude the most recent month.
