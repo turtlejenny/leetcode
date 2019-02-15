43d71386af776339350bfd39cdbd1aa5e6ab8a13
turtlejenny <31515354+turtlejenny@users.noreply.github.com>
2019-02-14 20:37:55 -0800

    Update Median  Employee Salary.sql

--- Median  Employee Salary.sql
+++ Median  Employee Salary.sql
@@ -36,14 +36,8 @@
 --+-----+------------+--------+
 --
 
-select t1.Id as Id, t1.Company, t1.Salary
-from Employee as t1 
-inner join Employee as t2
-on t1.Company = t2.Company
-group by t1.Id
-having abs(sum(CASE when t2.Salary<t1.Salary then 1
-                  when t2.Salary>t1.Salary then -1
-                  when t2.Salary=t1.Salary and t2.Id<t1.Id then 1
-                  when t2.Salary=t1.Salary and t2.Id>t1.Id then -1
-                  else 0 end)) <= 1
-order by t1.Company, t1.Salary, t1.Id;
+select e.Id, e.Company, e.salary
+from
+(select Id, Company, salary, ROW_NUMBER() over(partition by Company order by salary) as rnk, count(*) over(partition by Company) as cnt
+from employees) e
+where e.rnk in (CEILING(e.cnt/2.0), e.cnt/2+1);
